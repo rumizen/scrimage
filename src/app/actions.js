f689af9d0024f00484c616c610ea270b708d04c1
Steve Rumizen <steverumizen@gmail.com>
2024-02-13 14:41:59 -0500

    Update fetch action

--- src/app/actions.js
+++ src/app/actions.js
@@ -3,15 +3,17 @@
 export async function fetchImage(query) {
   const apiKey =
     "b10292a3720dbeaaebf2f3054ad9f2069802cf97bf5b33ffb76db0f185d693fe";
-  const queryString = `https://serpapi.com/search?engine=google_images&api_key=${apiKey}&safe=active&filter=0&q=${query}`;
-  const res = await fetch(queryString);
+    function buildQueryString(pageNumber) {
+      return `https://serpapi.com/search?engine=google_images&api_key=${apiKey}&safe=active&filter=0&ijn=${pageNumber}q=${query}`;
+    }
+  const res1 = await fetch(buildQueryString(0));
+
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
-  if (!res.ok) {
+  if (!res1.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
   }
-  console.log("fetch response:", res);
-  return res.json();
+  return res1.json();
 }
