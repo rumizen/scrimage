c21284593ca208b543265d3490251ec9e29d3883
Steve Rumizen <steverumizen@gmail.com>
2024-02-14 16:01:21 -0500

    Update file structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,55 +1,10 @@
 "use client";
-import { useState } from "react";
-import PlayerOne from "./ui/PlayerOne";
-import PlayerTwo from "./ui/PlayerTwo";
-import { fetchImage } from "@/app/actions";
+import GameWrapper from "./ui/GameWrapper";
 
-export default function GameWrapper() {
-  const [playerTurn, setPlayerTurn] = useState(1);
-  const [image, setImage] = useState({});
-  const [guessPlacement, setGuessPlacement] = useState(null);
-
-  function calculateGuessPlacement(results) {
-    const placement = results.find(
-      (result) => result.original === image.original
-    );
-    if (placement) {
-      setGuessPlacement(placement.position);
-    } else {
-      setGuessPlacement(0);
-    }
-  }
-
-  async function handleSubmit(query) {
-    const data = await fetchImage(query);
-    if (playerTurn === 1) {
-      setImage(data.images_results[0]);
-    }
-    if (playerTurn === 2) {
-      calculateGuessPlacement(data.images_results);
-    }
-  }
-
-  function advanceTurn(status) {
-    setPlayerTurn(status);
-  }
-
+export default function Home() {
   return (
     <>
-      {playerTurn === 1 && (
-        <PlayerOne
-          handleSubmit={handleSubmit}
-          image={image}
-          advanceTurn={advanceTurn}
-        />
-      )}
-      {playerTurn === 2 && (
-        <PlayerTwo
-          handleSubmit={handleSubmit}
-          image={image}
-          guessPlacement={guessPlacement}
-        />
-      )}
+      <GameWrapper />
     </>
   );
 }
